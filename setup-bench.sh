34157d44b2cc984d7e7a971c155f27f985142627
Andrea Cracco <guilucand@gmail.com>
2022-01-11 12:42:23 +0100

    Automatically build all needed tools

---
+++ setup-bench.sh
@@ -0,0 +1,46 @@
+
+cargo build --release
+
+mkdir building/
+mkdir tools/
+
+pushd building/
+
+    git clone https://github.com/Guilucand/biloki --recursive
+    git clone https://github.com/GATB/bcalm --recursive
+    git clone https://github.com/pmelsted/bifrost
+    git clone https://github.com/COMBINE-lab/cuttlefish cuttlefish2
+
+    pushd biloki/
+        cargo build --release
+    popd
+
+    pushd bcalm/
+        mkdir build
+        cd build
+        cmake ..
+        make -j
+    popd
+
+    pushd bifrost/
+        mkdir build
+        cd build
+        cmake ..
+        make -j
+    popd
+    cp bifrost/ tools
+
+
+    pushd cuttlefish2/
+        git checkout develop
+        mkdir build
+        cd build
+        cmake ..
+        make -j
+    popd
+popd
+
+cp building/biloki/target/release/BiLoki tools/
+cp building/bifrost/build/src/Bifrost tools/
+cp building/bcalm/build/bcalm tools/
+cp building/cuttlefish2/build/src/cuttlefish tools/
