f3dbaafa3a9803707720bcaa4973a8c64e84d89c
Andrea Cracco <guilucand@gmail.com>
2022-10-03 16:11:40 +0200

    .

--- run-colored-benchmarks.sh
+++ run-colored-benchmarks.sh
@@ -2,7 +2,7 @@
 
 mkdir tests-final/
 
-cargo run --release -- bench human-colored tests-final/human-colored-k27 --threads "$SLURM_CPUS_ON_NODE"
+cargo run --release -- bench human-colored-k27 tests-final/human-colored-k27 --threads "$SLURM_CPUS_ON_NODE"
 cargo run --release -- bench human-colored-k63 tests-final/human-colored-k63 --threads "$SLURM_CPUS_ON_NODE"
 
 cargo run --release -- bench colored-salmonella-big tests-final/colored-salmonella-big --threads "$SLURM_CPUS_ON_NODE"
