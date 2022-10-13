7cfaab80458cbc61ce95c501427bd77878727c45
Andrea Cracco <guilucand@gmail.com>
2022-10-13 10:48:43 +0200

    Fix

--- aurinko-slurm-test-g.sh
+++ aurinko-slurm-test-g.sh
@@ -2,11 +2,11 @@
 #
 #SBATCH --job-name=article-tests-g
 #SBATCH --output=article-tests-g.txt
-#SBATCH --cpus-per-task=200
+#SBATCH --cpus-per-task=199
 #SBATCH --ntasks=1
 #SBATCH --time=3000:00
 #SBATCH --mem=524288
 
-for threads in {192,128,64,32,16,8,4,2,1}; do
+for threads in {199, 192,128,64,32,16,8,4,2,1}; do
     srun --cpus-per-task $threads ./aurinko-single-run-test-g.sh $threads
 done
